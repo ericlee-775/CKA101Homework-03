ee534df0a23f1f5ca985416f85b8cddcb1969650
Ray Ray <ericlee20030829@gmail.com>
2026-04-08 21:18:20 +0800

    f 5_3

---
+++ src/hw5/home_work5_3/MyRectangle.java
@@ -0,0 +1,24 @@
+package hw5.home_work5_3;
+
+public class MyRectangle {
+	private double width;
+	private double depth;
+	
+	public MyRectangle() {};
+	public MyRectangle(double width,double depth) {
+		this.width=width;
+		this.depth=depth;
+		
+	};
+	
+	public void setWidth(double width) {
+		this.width=width;
+	}
+	public void setDepth(double depth) {
+		this.depth=depth;
+	}
+	public double getArea() {
+		return width*depth;
+	}
+	
+}
---
+++ src/hw5/home_work5_3/MyRectangleMain.java
@@ -0,0 +1,8 @@
+package hw5.home_work5_3;
+
+public class MyRectangleMain {
+	public static void main(String[] args) {
+		MyRectangle R1 = new MyRectangle(5,3);
+		System.out.println(R1.getArea());
+	}
+}
